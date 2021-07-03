2b33ec94470faad5d3f60dc4dd192d0e1c73124d
Eveneko <huyubin0203@gmail.com>
2021-07-03 21:31:04 +0800

    剑指Offer: 07 递归法

--- README.md
+++ README.md
@@ -6,4 +6,7 @@ Online Judge Problems‘ AC Code
 [2039](http://bailian.openjudge.cn/practice/2039?lang=en_US): [Code](https://github.com/Eveneko/Awesome-Code/blob/master/OpenJ_Bailian/2039.cpp) |
 [2775](http://bailian.openjudge.cn/practice/2775?lang=en_US): [Code](https://github.com/Eveneko/Awesome-Code/blob/master/OpenJ_Bailian/2775.cpp) |
 [4103](http://bailian.openjudge.cn/practice/4103?lang=en_US): [Code](https://github.com/Eveneko/Awesome-Code/blob/master/OpenJ_Bailian/4103.cpp) |
-[4104](http://bailian.openjudge.cn/practice/4104?lang=en_US): [Code](https://github.com/Eveneko/Awesome-Code/blob/master/OpenJ_Bailian/4104.cpp) |
+[4104](http://bailian.openjudge.cn/practice/4104?lang=en_US): [Code](https://github.com/Eveneko/Awesome-Code/blob/master/OpenJ_Bailian/4104.cpp) 
+
+## 剑指Offer
+[07. 重建二叉树](https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof/): [Code](https://github.com/Eveneko/Awesome-Code/blob/master/剑指Offer/07.cpp) 
---
+++ 剑指Offer/07.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, int> indexmap;
+    TreeNode* gen(vector<int>& preorder, int prestart, int preend, vector<int>& inorder, int instart, int inend) {
+        if(prestart <= preend && instart <= inend){
+            TreeNode *newnode = new TreeNode(preorder[prestart]);
+            int offset = indexmap[preorder[prestart]] - instart;
+            newnode->left = gen(preorder, prestart+1, prestart+offset, inorder, instart, instart+offset-1);
+            newnode->right = gen(preorder, prestart+offset+1, preend, inorder, instart+offset+1, inend);
+            return newnode;
+        }
+        return nullptr;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for(int i = 0; i < inorder.size(); i++){
+            indexmap[inorder[i]] = i;
+        }
+        return gen(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1);
+    }
+};
