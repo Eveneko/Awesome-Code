c4bcd56e02da27040c2b0e423ab16551bd18bf0d
Eveneko <huyubin0203@gmail.com>
2021-07-29 17:05:00 +0800

    剑指offer: 35. 复杂链表的复制

--- 剑指Offer/35.cpp
+++ 剑指Offer/35.cpp
@@ -1,4 +1,75 @@
 /**
- * 34. 二叉树中和为某一值的路径
- * https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/
+ * 35. 复杂链表的复制
+ * https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof/
  */
+
+#include<stdio.h>
+#include<unordered_map>
+using namespace std;
+
+/*
+ * Definition for a Node.
+ */
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == nullptr) return nullptr;
+        unordered_map<Node*, Node*> map;
+        Node* cur = head;
+        while(cur != nullptr){
+            map[cur] = new Node(cur->val);
+            cur = cur->next;
+        }
+        cur = head;
+        while(cur != nullptr){
+            map[cur]->next = map[cur->next];
+            map[cur]->random = map[cur->random];
+            cur = cur->next;
+        }
+        return map[head];
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == nullptr) return nullptr;
+        Node* cur = head;
+        while(cur != nullptr){
+            Node* tmp = new Node(cur->val);
+            tmp->next = cur->next;
+            cur->next = tmp;
+            cur = tmp->next;
+        }
+        cur = head;
+        while(cur != nullptr){
+            if(cur->random != nullptr){
+                cur->next->random = cur->random->next;
+            }
+            cur = cur->next->next;
+        }
+        cur = head->next;
+        Node* res = head->next;
+        Node* pre = head;
+        while(cur->next != nullptr){
+            pre->next = pre->next->next;
+            cur->next = cur->next->next;
+            pre = pre->next;
+            cur = cur->next;
+        }
+        pre->next = nullptr;
+        return res;
+    }
+};
