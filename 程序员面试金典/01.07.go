7a12cd23f8f1f9dcb85559241a037959bdf857c6
Eveneko <huyubin0203@gmail.com>
2022-05-24 14:10:23 +0800

    面试题 01.07. 旋转矩阵

---
+++ 程序员面试金典/01.07.go
@@ -0,0 +1,20 @@
+func rotate(matrix [][]int)  {
+    n := len(matrix)
+    for i := 0; i < n/2; i++ {
+        matrix[i], matrix[n-1-i] = matrix[n-1-i], matrix[i]
+    }
+    for i := 0; i < n; i++ {
+        for j := 0; j < i; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
+    }
+}
+
+func rotate(matrix [][]int)  {
+    n := len(matrix)
+    for i := 0; i < n/2; i++ {
+        for j := 0; j < (n+1)/2; j++ {
+            matrix[i][j], matrix[n-1-j][i], matrix[n-1-i][n-1-j], matrix[j][n-1-i] = matrix[n-1-j][i], matrix[n-1-i][n-1-j], matrix[j][n-1-i], matrix[i][j]
+        }
+    }
+}
